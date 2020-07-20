a45de4a470a07506d828728be1aace70af2c2f8e
svalenzuela <59404624+svalenzuela1@users.noreply.github.com>
2020-07-20 12:56:55 -0400

    final changes

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 console.log($)
 
 let url = 'https://spreadsheets.google.com/feeds/list/1UUsLTR3KfDVDztVxiJ9pC61R8jDDpiVVilF-fxHzp4U/od6/public/values?alt=json'
@@ -66,55 +67,5 @@ const showMenu = (event) => {
 $hamburger.on("click", showMenu)
 
 
-/*
-//skills section
-const githubImg = "https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png";
-//let clickOnGithub = () => {
-    const $gitLogo = $('<img>')
-    //const githubImg = 'https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png';
-    $gitLogo.attr('src', "https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png")
-    $('.projects').append($gitLogo)
-    $gitLogo.on('click', (event) => {
-location.replace("https://github.com/svalenzuela1")
-    }
-*/
-//})
-//}
+document.getElementById('submit')
 
- //$githubImg.on('click',(event) => {
-  //  clickOnGithub()
-//})
-
-/*
-.then( response => response.json() )    // returns the JSON data as a JS object
-    .then( data => {
-        // creates an array of parsed project objects
-        const projects = data.feed.entry.map( entry => {
-             return {
-                title: entry.gsx$title.$t,
-                image: entry.gsx$image.$t,
-                description: entry.gsx$description.$t,
-                url: entry.gsx$url.$t
-             }
-         })
-         
-         app(projects)  // calls the application with your new projects array as argument
-    })
-// all application logic is included inside the app() function    
-const app = (projects) => {
-    // creates a jQuery DOM element based on an individual project object
-    const createProjectElement = (project) => {
-        const $div = $('<div>').addClass("project")
-        $div.append($('<h2>').text(project.title))
-        $div.append($('<img>').attr('src', project.image))
-        $div.append($('<p>').addClass("description").text(project.description))
-        $div.append($('<a>').attr('href', project.url).text("LINK >>"))
-        return $div
-    }
-    // adds each project element to <body>
-    projects.forEach(project => {
-        const $projectDiv = createProjectElement(project)
-        $('body').append($projectDiv)
-    })
-}
-*/
